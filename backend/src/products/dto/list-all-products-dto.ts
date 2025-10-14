a5cbbece6809a4c9fbb92b5d9414dbede744f6e0
Bruce Lee <brucelee92371@gmail.com>
2025-10-15 10:36:48 +1100

    Transform query params from string

--- backend/src/products/dto/list-all-products-dto.ts
+++ backend/src/products/dto/list-all-products-dto.ts
@@ -1,11 +1,14 @@
+import { Transform } from "class-transformer";
 import { IsInt, IsOptional, Min } from "class-validator";
 
 export class ListAllProductsDto {
+  @Transform(({ value }) => parseInt(value))
   @IsInt({ message: "Offset must be an integer" })
   @Min(0, { message: "Offset must be be greater than or equal to 0" })
   @IsOptional()
   offset: number;
 
+  @Transform(({ value }) => parseInt(value))
   @IsInt({ message: "Limit must be an integer" })
   @Min(0, { message: "Limit must be be greater than or equal to 0" })
   @IsOptional()
