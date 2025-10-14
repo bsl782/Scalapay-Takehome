36d5e416e1add89ec8f8b010f2da5cb648110ebd
Bruce Lee <brucelee92371@gmail.com>
2025-10-15 10:25:37 +1100

    spacing

--- backend/src/products/dto/create-product.dto.ts
+++ backend/src/products/dto/create-product.dto.ts
@@ -1,4 +1,10 @@
-import { IsInt, IsNotEmpty, IsNumber, IsString, Min } from "class-validator";
+import {
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Min,
+} from "class-validator";
 
 export class CreateProductDto {
   @IsString({ message: "Product Token must be a string" })
@@ -10,10 +16,10 @@ export class CreateProductDto {
   name: string;
 
   @IsNumber({}, { message: "Price must be a number" })
-  @Min(0, { message: "Price must be at least 0" })
+  @Min(0, { message: "Price must be greater than or equal to 0" })
   price: number;
 
   @IsInt({ message: "Stock must be an integer" })
-  @Min(0, { message: "Stock must be be greater than or equal to 0" })
+  @Min(0, { message: "Stock must be greater than or equal to 0" })
   stock: number;
 }
