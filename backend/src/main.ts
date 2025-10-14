a0d1837e67338123732a6492b704ffeffc39a6d4
Bruce Lee <brucelee92371@gmail.com>
2025-10-15 10:49:35 +1100

    Filtering removal

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,11 @@
-import 'reflect-metadata';
+import "reflect-metadata";
 import { ValidationPipe } from "@nestjs/common/pipes/validation.pipe";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { HttpExceptionFilter } from "./common/filters/http-exception-filter";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
