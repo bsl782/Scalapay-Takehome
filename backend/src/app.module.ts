6543d0e78fd094fcd90580f9097e49f7a5fe0947
Bruce Lee <brucelee92371@gmail.com>
2025-10-14 22:36:05 +1100

    db config setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,20 +1,30 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { SequelizeModule } from "@nestjs/sequelize";
 import { ProductsController } from "./products/products.controller";
 import { ProductsModule } from "./products/products.module";
 import { ProductsService } from "./products/products.service";
 
 @Module({
-	imports: [ProductsModule, SequelizeModule.forRoot({
-		dialect: "mysql",
-		host: "localhost",
-		port: 3306,
-		username: "user",
-		password: "password",
-		database: "ecommerce",
-		autoLoadModels: true,
-		synchronize: true,
-	})],
+	imports: [
+		// Load .env file
+		ConfigModule.forRoot({
+			envFilePath: "../.env",
+		}),
+		// Connect to database
+		SequelizeModule.forRoot({
+			dialect: "mysql",
+			host: process.env.DB_HOST,
+			port: Number(process.env.DB_PORT),
+			username: process.env.DB_USER,
+			password: process.env.DB_PASSWORD,
+			database: process.env.DB_NAME,
+			autoLoadModels: true,
+			synchronize: true,
+		}),
+		// Import modules
+		ProductsModule,
+	],
 	controllers: [ProductsController],
 	providers: [ProductsService],
 })
