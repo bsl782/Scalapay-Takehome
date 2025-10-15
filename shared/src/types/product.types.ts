2b375d523016a7de170b004ae189b81547d09bc7
Bruce Lee <brucelee92371@gmail.com>
2025-10-15 17:37:13 +1100

    reduced interfaces

--- shared/src/types/product.types.ts
+++ shared/src/types/product.types.ts
@@ -2,7 +2,6 @@
  * Core Product entity interface
  */
 export interface Product {
-  id: number;
   productToken: string;
   name: string;
   price: number;
@@ -18,23 +17,4 @@ export interface ApiError {
   message: string | string[];
   error: string;
   statusCode: number;
-}
-
-/**
- * Paginated response interface
- */
-export interface PaginatedResponse<T> {
-  products: T[];
-  productsCount: number;
-  offset: number;
-  limit: number;
-}
-
-/**
- * API Response wrapper
- */
-export interface ApiResponse<T = unknown> {
-  data?: T;
-  message?: string;
-  success: boolean;
 }
