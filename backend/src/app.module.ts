3dd591c4bc78fd2140731b4c5d1016d217220b81
Bruce Lee <brucelee92371@gmail.com>
2025-10-15 12:54:44 +1100

    dynamic env files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,22 +4,24 @@ import { SequelizeModule } from "@nestjs/sequelize";
 import { ProductsModule } from "./products/products.module";
 
 @Module({
-	imports: [
-		// Load .env file
-		ConfigModule.forRoot(),
-		// Connect to database
-		SequelizeModule.forRoot({
-			dialect: "mysql",
-			host: process.env.DB_HOST,
-			port: Number(process.env.DB_PORT),
-			username: process.env.DB_USER,
-			password: process.env.DB_PASSWORD,
-			database: process.env.DB_NAME,
-			autoLoadModels: true,
-			synchronize: true,
-		}),
-		// Import modules
-		ProductsModule,
-	],
+  imports: [
+    // Load .env file
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV || "local"}`,
+    }),
+    // Connect to database
+    SequelizeModule.forRoot({
+      dialect: "mysql",
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    // Import modules
+    ProductsModule,
+  ],
 })
 export class AppModule {}
