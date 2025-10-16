02f4bf36fb262a1df47572c3bfc98a537ef014e1
Bruce Lee <brucelee92371@gmail.com>
2025-10-16 14:36:58 +1100

    removed comments

--- shared/src/types/product.types.ts
+++ shared/src/types/product.types.ts
@@ -1,6 +1,4 @@
-/**
- * Core Product entity interface
- */
+
 export interface Product {
   id: number;
   productToken: string;
@@ -11,9 +9,6 @@ export interface Product {
   updatedAt: string;
 }
 
-/**
- * API Error response interface
- */
 export interface ApiError {
   message: string | string[];
   error: string;
